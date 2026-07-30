a9a2e7b707c218ab314cf7cbe1dc0aef46c345ab
Yukon163 <18096690703@163.com>
2026-07-31 02:55:53 +0800

    chore(release): bump version to 3.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.ahu.ahutong"
         minSdk = 26
         targetSdk = 36
-        versionCode = 320
-        versionName = "3.2.0"
+        versionCode = 321
+        versionName = "3.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a")
